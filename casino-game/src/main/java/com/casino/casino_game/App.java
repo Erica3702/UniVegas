cd550a524e3caa9e34ea9790cd0efa325e374b84
LucaCattaneoo <l.cattaneo21@studenti.unibg.it>
2024-12-13 14:33:16 +0100

    aggiunta carte

--- casino-game/src/main/java/com/casino/casino_game/App.java
+++ casino-game/src/main/java/com/casino/casino_game/App.java
@@ -6,23 +6,24 @@ import org.sqlite.core.DB;
 import java.sql.Connection;
 
 public class App {
-    // Attributo della classe DBConnection
+    
     private static DBConnection db;
 
     public static void main(String[] args) {
-        // Crea un'istanza della classe DBConnection
         db = new DBConnection();
+        db.testQuery(); //collegamento db e test query
         
-        // Chiamata al metodo connect()
-        Connection connection = db.connect();
-
-        // Verifica la connessione
-        if (connection != null) {
-          System.out.println("Database connection established successfully!");
-        } else {
-          System.out.println("Failed to establish database connection.");
-        }
         
-        db.testQuery();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
