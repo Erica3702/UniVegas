01f747395af81a628d4932004a17ee663f3f7d7d
TacuriRodrigo <145766154+TacuriRodrigo@users.noreply.github.com>
2025-01-22 18:31:36 +0100

    aggiunta classi User dei vari giochi
    aggiunta

---
+++ casino-game/src/main/java/com/casino/model/UserPoker.java
@@ -0,0 +1,18 @@
+package com.casino.model;
+
+import java.util.List;
+
+public class UserPoker extends User{
+
+	public UserPoker(String username, String password, int token, List<Card> mano) {
+		super(username, password, token, mano);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int calcolaValoreMano() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
